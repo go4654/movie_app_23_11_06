3a2335b69a012a8ad4dc7062bbfb063edcacff0d
go4654 <go4654@naver.com>
2023-11-17 11:09:37 +0900

    header event

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 import { routes } from "../routes";
 import styled from "styled-components";
 import { mainColors } from "../style/GlobalStyled";
+import { useEffect, useRef } from "react";
 
 const SHeader = styled.header`
   width: 100%;
   padding: 20px 5%;
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   display: flex;
@@ -33,8 +34,26 @@ const Nav = styled.ul`
 `;
 
 export const Header = () => {
+  const headerRef = useRef(null);
+
+  const scrollHandler = () => {
+    const pageY = window.scrollY;
+    if (pageY > 300) {
+      headerRef.current.style.position = "fixed";
+      headerRef.current.style.backgroundColor = "rgba(0,0,0,0.7)";
+      headerRef.current.style.backdropFilter = "blur(2px)";
+    } else {
+      headerRef.current.style.position = "absolute";
+      headerRef.current.style.backgroundColor = "transparent";
+    }
+  };
+
+  useEffect(() => {
+    return window.addEventListener("scroll", scrollHandler);
+  }, []);
+
   return (
-    <SHeader>
+    <SHeader ref={headerRef}>
       <Logo>
         <Link to={routes.home}>MOIVE</Link>
       </Logo>
